6c5999d9f72a229e3e4cbedcda6ee73164d82dde
shivani <shivanisamant981@gmail.com>
2025-09-06 21:56:54 +0530

    done till prodcut management list_product function

--- stockManagement.py
+++ stockManagement.py
@@ -1,7 +1,9 @@
 #har har mahadev
 
 #Stock management system
-import os
+import datetime
+now=datetime.datetime.now()
+
 import mysql.connector
 
 def product_management():
@@ -86,21 +88,123 @@ def database_management():
 def create_database():
     mydb=mysql.connector.connect(host="localhost" ,user="root", passwd="743211",database="stock")
     mycursor=mydb.cursor()
+    #product table
     print("Creating product table")
-    sql="CREATE TABLE if not exists prodcut (\
+    sql="CREATE TABLE if not exists product (\
         pcode int (4) PRIMARY KEY,\
         pname char(30) NOT NULL,\
         pprice float(8,2),\
         pqty int(4),\
         pcat char(30));"
     mycursor.execute(sql)
+    # order table
+    print("Crating ORDER TABLE")
+    sql="CREATE TABLE if not exists orders(\
+    orderid int(4) PRIMARY KEY,\
+    orderdate DATE,\
+    pcode char(30) NOT NULL,\
+    pprice float(8,2),\
+    pqty int(4),\
+    supplier char(30)); "
+    mycursor.execute(sql)
+    print("ORDER table created")
+    
+    #sales table
+    
+    sql="CREATE TABLE  if not exists sales(\
+        salesid int(4) PRIMARY KEY,\
+        salesdate DATE,\
+        pcode int(4) references product(pcode),\
+        pprice float(8,2),\
+        pqty int(4),\
+        total double(8,2));"
+    mycursor.execute(sql)
+    print("SALES table created")
+
+    #user table
+    sql="CREATE TABLE if not exists user(\
+            uid char(6) PRIMARY KEY,\
+            uname char(30) NOT NULL,\
+            upwd char(30));"
+    mycursor.execute(sql)
+    print("USER table created")
+    
+
+def list_database():
+    mydb=mysql.connector.connect(host="localhost", user="root", passwd="743211" , database="stock")
+    mycursor=mydb.cursor()
+    sql="show tables"
+    mycursor.execute(sql)
+    for i in mycursor:
+        print (i)
+
+
+def add_order():
+    mydb=mysql.connector.connect(host="localhost",user="root",password="743211",database="stock")
+    mycursor=mydb.cursor()
+    now=datetime.datetime.now()
+    sql="INSERT INTO orders( orderid,orderdate ,pcode,pprice ,pqty ,supplier) value(%s,%s,%s,%s,%s,%s)"
+    code=int (input("enter the product code:"))
+    oid=now.year+now.month+now.day+now.hour+now.minute+now.second
+    qty=int(input("enter product quantity"))
+    price=float(input("Enter the product unit price"))
+    supplier=input("Enter supplier details")
+    val=(oid,now,code,price,qty,supplier)
+    mycursor.execute(sql, val)
+    mydb.commit()
+def list_order():
+    mydb=mysql.connector.connect(host="localhost", user="root", passwd="743211",database="stock")
+    mycursor=mydb.cursor()
+    sql="SELECT * from orders"
+    mycursor.execute(sql)
+    print("\t\t\t\t\t ORDER DETAILS")
+    print("-"*85)
+    for i in mycursor:
+        print("orderID\t orderdate \t pcode \t pprice  \t pqty \t supplier")
+        print(i[0],"\t",i[1],"\t",i[2],"\t",i[3],"\t",i[4],"\t",i[5])
+    print("-"*85)
+    
+def add_product():
+    mydb=mysql.connector.connect(host="localhost",user="root",passwd="743211",database="stock")
+    mycursor=mydb.cursor()
+    sql="INSERT INTO product(pcode , pname,pprice, pqty,pcat)values(%s,%s,%s,%s,%s)"
+    code=int(input("\t\tEnter product code"))
+    search="SELECT count(*) From product where pcode=%s;"
+    val=(code,)
+    mycursor.execute(search,val)
+    for x in mycursor:
+        cnt=x[0]
+    if cnt==0:
+        name=input("\t\tenter the product name")
+        qty=int(input("Enter product quantity"))
+        price=float(input("\t\tEnter product unit price"))
+        cat=input("\t\t Enter product category")
+        val=(code,name,price, qty, cat)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    else:
+        print("\t\tProduct already exist")
         
+def list_product():
+    mydb=mysql.connector.connect(host="localhost",user="root",passwd="743211",database="stock")
+    mycursor=mydb.cursor()
+    sql="SELECT * FROM PRODUCT"
+    mycursor.execute(sql)
+    print("\t\t\t\t PRODUCT DETAILS")
+    print("\t\t","-"*47)
+    print("\t\t code name price quantity category")
+    print("\t\t","-"*47)
+    for i in mycursor:
+        print("\t\t",i[0],"\t",i[1],"\t",i[2],"\t",i[3],"\t",i[4])
+    print("\t\t","-"*47)
+
+def update_product():
 
         
 
  
 while(True):
-    os.system("cls")
+    
     print("\t\t\t STOCK MANAGEMENT SYSTEM ")
     print("\t\t\t ***********************************\t")
     print("\t\t\t 1. PRODUCT MANAGEMENT ")
